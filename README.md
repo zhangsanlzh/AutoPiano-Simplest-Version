bf5b65242389a731d3c32cf650355f6a8f43bb01
Luskyle <844804539@qq.com>
2019-11-13 20:51:48 +0800

    Create the Repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# AutoPiano-Electron
+
+
+> ## 简介
+
+这是项目 [AutoPiano](https://github.com/WarpPrism/AutoPiano) 的 Electron 版本。抽出了核心功能，可以不必通过`npm install`, `npm run`的方式运行，是最简版本。
+
+![README.png](./README.png)
+
+
---
+++ js/piano.js
@@ -0,0 +1,362 @@
+
+baseUrl="sounds/"
+var Notes=[
+  {id: 1, name: 'C2', keyCode: '49', key: '1', url: baseUrl + 'a49.mp3', type: 'white'},
+  {id: 2, name: 'D2', keyCode: '50', key: '2', url: baseUrl + 'a50.mp3', type: 'white'},
+  {id: 3, name: 'E2', keyCode: '51', key: '3', url: baseUrl + 'a51.mp3', type: 'white'},
+  {id: 4, name: 'F2', keyCode: '52', key: '4', url: baseUrl + 'a52.mp3', type: 'white'},
+  {id: 5, name: 'G2', keyCode: '53', key: '5', url: baseUrl + 'a53.mp3', type: 'white'},
+  {id: 6, name: 'A2', keyCode: '54', key: '6', url: baseUrl + 'a54.mp3', type: 'white'},
+  {id: 7, name: 'B2', keyCode: '55', key: '7', url: baseUrl + 'a55.mp3', type: 'white'},
+  {id: 8, name: 'C3', keyCode: '56', key: '8', url: baseUrl + 'a56.mp3', type: 'white'},
+  {id: 9, name: 'D3', keyCode: '57', key: '9', url: baseUrl + 'a57.mp3', type: 'white'},
+  {id: 10, name: 'E3', keyCode: '48', key: '0', url: baseUrl + 'a48.mp3', type: 'white'},
+  {id: 26, name: 'F3', keyCode: '81', key: 'Q', url: baseUrl + 'a81.mp3', type: 'white'},
+  {id: 32, name: 'G3', keyCode: '87', key: 'W', url: baseUrl + 'a87.mp3', type: 'white'},
+  {id: 14, name: 'A3', keyCode: '69', key: 'E', url: baseUrl + 'a69.mp3', type: 'white'},
+  {id: 27, name: 'B3', keyCode: '82', key: 'R', url: baseUrl + 'a82.mp3', type: 'white'},
+  {id: 29, name: 'C4', keyCode: '84', key: 'T', url: baseUrl + 'a84.mp3', type: 'white'},
+  {id: 34, name: 'D4', keyCode: '89', key: 'Y', url: baseUrl + 'a89.mp3', type: 'white'},
+  {id: 30, name: 'E4', keyCode: '85', key: 'U', url: baseUrl + 'a85.mp3', type: 'white'},
+  {id: 18, name: 'F4', keyCode: '73', key: 'I', url: baseUrl + 'a73.mp3', type: 'white'},
+  {id: 24, name: 'G4', keyCode: '79', key: 'O', url: baseUrl + 'a79.mp3', type: 'white'},
+  {id: 25, name: 'A4', keyCode: '80', key: 'P', url: baseUrl + 'a80.mp3', type: 'white'},
+  {id: 10, name: 'B4', keyCode: '65', key: 'A', url: baseUrl + 'a65.mp3', type: 'white'},
+  {id: 28, name: 'C5', keyCode: '83', key: 'S', url: baseUrl + 'a83.mp3', type: 'white'},
+  {id: 13, name: 'D5', keyCode: '68', key: 'D', url: baseUrl + 'a68.mp3', type: 'white'},
+  {id: 15, name: 'E5', keyCode: '70', key: 'F', url: baseUrl + 'a70.mp3', type: 'white'},
+  {id: 16, name: 'F5', keyCode: '71', key: 'G', url: baseUrl + 'a71.mp3', type: 'white'},
+  {id: 17, name: 'G5', keyCode: '72', key: 'H', url: baseUrl + 'a72.mp3', type: 'white'},
+  {id: 19, name: 'A5', keyCode: '74', key: 'J', url: baseUrl + 'a74.mp3', type: 'white'},
+  {id: 20, name: 'B5', keyCode: '75', key: 'K', url: baseUrl + 'a75.mp3', type: 'white'},
+  {id: 21, name: 'C6', keyCode: '76', key: 'L', url: baseUrl + 'a76.mp3', type: 'white'},
+  {id: 35, name: 'D6', keyCode: '90', key: 'Z', url: baseUrl + 'a90.mp3', type: 'white'},
+  {id: 33, name: 'E6', keyCode: '88', key: 'X', url: baseUrl + 'a88.mp3', type: 'white'},
+  {id: 12, name: 'F6', keyCode: '67', key: 'C', url: baseUrl + 'a67.mp3', type: 'white'},
+  {id: 31, name: 'G6', keyCode: '86', key: 'V', url: baseUrl + 'a86.mp3', type: 'white'},
+  {id: 11, name: 'A6', keyCode: '66', key: 'B', url: baseUrl + 'a66.mp3', type: 'white'},
+  {id: 23, name: 'B6', keyCode: '78', key: 'N', url: baseUrl + 'a78.mp3', type: 'white'},
+  {id: 22, name: 'C7', keyCode: '77', key: 'M', url: baseUrl + 'a77.mp3', type: 'white'},
+  {id: 36, name: 'C#2', keyCode: 'b49', key: '⇧<br>+<br>1', url: baseUrl + 'b49.mp3', type: 'black'},
+  {id: 37, name: 'D#2', keyCode: 'b50', key: '⇧<br>+<br>2', url: baseUrl + 'b50.mp3', type: 'black'},
+  {id: 38, name: 'F#2', keyCode: 'b52', key: '⇧<br>+<br>4', url: baseUrl + 'b52.mp3', type: 'black'},
+  {id: 39, name: 'G#2', keyCode: 'b53', key: '⇧<br>+<br>5', url: baseUrl + 'b53.mp3', type: 'black'},
+  {id: 40, name: 'A#2', keyCode: 'b54', key: '⇧<br>+<br>6', url: baseUrl + 'b54.mp3', type: 'black'},
+  {id: 41, name: 'C#3', keyCode: 'b56', key: '⇧<br>+<br>8', url: baseUrl + 'b56.mp3', type: 'black'},
+  {id: 42, name: 'D#3', keyCode: 'b57', key: '⇧<br>+<br>9', url: baseUrl + 'b57.mp3', type: 'black'},
+  {id: 43, name: 'F#3', keyCode: 'b81', key: '⇧<br>+<br>Q', url: baseUrl + 'b81.mp3', type: 'black'},
+  {id: 44, name: 'G#3', keyCode: 'b87', key: '⇧<br>+<br>W', url: baseUrl + 'b87.mp3', type: 'black'},
+  {id: 45, name: 'A#3', keyCode: 'b69', key: '⇧<br>+<br>E', url: baseUrl + 'b69.mp3', type: 'black'},
+  {id: 46, name: 'C#4', keyCode: 'b84', key: '⇧<br>+<br>T', url: baseUrl + 'b84.mp3', type: 'black'},
+  {id: 47, name: 'D#4', keyCode: 'b89', key: '⇧<br>+<br>Y', url: baseUrl + 'b89.mp3', type: 'black'},
+  {id: 48, name: 'F#4', keyCode: 'b73', key: '⇧<br>+<br>I', url: baseUrl + 'b73.mp3', type: 'black'},
+  {id: 49, name: 'G#4', keyCode: 'b79', key: '⇧<br>+<br>O', url: baseUrl + 'b79.mp3', type: 'black'},
+  {id: 50, name: 'A#4', keyCode: 'b80', key: '⇧<br>+<br>P', url: baseUrl + 'b80.mp3', type: 'black'},
+  {id: 51, name: 'C#5', keyCode: 'b83', key: '⇧<br>+<br>S', url: baseUrl + 'b83.mp3', type: 'black'},
+  {id: 52, name: 'D#5', keyCode: 'b68', key: '⇧<br>+<br>D', url: baseUrl + 'b68.mp3', type: 'black'},
+  {id: 53, name: 'F#5', keyCode: 'b71', key: '⇧<br>+<br>G', url: baseUrl + 'b71.mp3', type: 'black'},
+  {id: 54, name: 'G#5', keyCode: 'b72', key: '⇧<br>+<br>H', url: baseUrl + 'b72.mp3', type: 'black'},
+  {id: 55, name: 'A#5', keyCode: 'b74', key: '⇧<br>+<br>J', url: baseUrl + 'b74.mp3', type: 'black'},
+  {id: 56, name: 'C#6', keyCode: 'b76', key: '⇧<br>+<br>L', url: baseUrl + 'b76.mp3', type: 'black'},
+  {id: 57, name: 'D#6', keyCode: 'b90', key: '⇧<br>+<br>Z', url: baseUrl + 'b90.mp3', type: 'black'},
+  {id: 58, name: 'F#6', keyCode: 'b67', key: '⇧<br>+<br>C', url: baseUrl + 'b67.mp3', type: 'black'},
+  {id: 59, name: 'G#6', keyCode: 'b86', key: '⇧<br>+<br>V', url: baseUrl + 'b86.mp3', type: 'black'},
+  {id: 60, name: 'A#6', keyCode: 'b66', key: '⇧<br>+<br>B', url: baseUrl + 'b66.mp3', type: 'black'},
+]
+
+var NotesMap = [
+  { name: 'C2', file: 'a49.mp3' },
+  { name: 'D2', file: 'a50.mp3' },
+  { name: 'E2', file: 'a51.mp3' },
+  { name: 'F2', file: 'a52.mp3' },
+  { name: 'G2', file: 'a53.mp3' },
+  { name: 'A2', file: 'a54.mp3' },
+  { name: 'B2', file: 'a55.mp3' },
+  { name: 'C3', file: 'a56.mp3' },
+  { name: 'D3', file: 'a57.mp3' },
+  { name: 'E3', file: 'a48.mp3' },
+  { name: 'F3', file: 'a81.mp3' },
+  { name: 'G3', file: 'a87.mp3' },
+  { name: 'A3', file: 'a69.mp3' },
+  { name: 'B3', file: 'a82.mp3' },
+  { name: 'C4', file: 'a84.mp3' },
+  { name: 'D4', file: 'a89.mp3' },
+  { name: 'E4', file: 'a85.mp3' },
+  { name: 'F4', file: 'a73.mp3' },
+  { name: 'G4', file: 'a79.mp3' },
+  { name: 'A4', file: 'a80.mp3' },
+  { name: 'B4', file: 'a65.mp3' },
+  { name: 'C5', file: 'a83.mp3' },
+  { name: 'D5', file: 'a68.mp3' },
+  { name: 'E5', file: 'a70.mp3' },
+  { name: 'F5', file: 'a71.mp3' },
+  { name: 'G5', file: 'a72.mp3' },
+  { name: 'A5', file: 'a74.mp3' },
+  { name: 'B5', file: 'a75.mp3' },
+  { name: 'C6', file: 'a76.mp3' },
+  { name: 'D6', file: 'a90.mp3' },
+  { name: 'E6', file: 'a88.mp3' },
+  { name: 'F6', file: 'a67.mp3' },
+  { name: 'G6', file: 'a86.mp3' },
+  { name: 'A6', file: 'a66.mp3' },
+  { name: 'B6', file: 'a78.mp3' },
+  { name: 'C7', file: 'a77.mp3' },
+
+  { name: 'C#2', file: 'b49.mp3' },
+  { name: 'D#2', file: 'b50.mp3' },
+  { name: 'F#2', file: 'b52.mp3' },
+  { name: 'G#2', file: 'b53.mp3' },
+  { name: 'A#2', file: 'b54.mp3' },
+  { name: 'C#3', file: 'b56.mp3' },
+  { name: 'D#3', file: 'b57.mp3' },
+  { name: 'F#3', file: 'b81.mp3' },
+  { name: 'G#3', file: 'b87.mp3' },
+  { name: 'A#3', file: 'b69.mp3' },
+  { name: 'C#4', file: 'b84.mp3' },
+  { name: 'D#4', file: 'b89.mp3' },
+  { name: 'F#4', file: 'b73.mp3' },
+  { name: 'G#4', file: 'b79.mp3' },
+  { name: 'A#4', file: 'b80.mp3' },
+  { name: 'C#5', file: 'b83.mp3' },
+  { name: 'D#5', file: 'b68.mp3' },
+  { name: 'F#5', file: 'b71.mp3' },
+  { name: 'G#5', file: 'b72.mp3' },
+  { name: 'A#5', file: 'b74.mp3' },
+  { name: 'C#6', file: 'b76.mp3' },
+  { name: 'D#6', file: 'b90.mp3' },
+  { name: 'F#6', file: 'b67.mp3' },
+  { name: 'G#6', file: 'b86.mp3' },
+  { name: 'A#6', file: 'b66.mp3' }
+]
+
+let mapFile = (name) => {
+  let file = ''
+    NotesMap.forEach(note => {
+      if (note.name == name) {
+        file = baseUrl + note.file
+      }
+    })
+    return file
+  }
+
+var SoundLib = (function() {
+  function SoundLib(){
+    
+  }
+
+  onload = null
+  SoundLib.Piano = {
+    'A2': mapFile('A2'),
+    'A3': mapFile('A3'),
+    'A4': mapFile('A4'),
+    'A5': mapFile('A5'),
+    'A6': mapFile('A6'),
+
+    'A#3': mapFile('A#3'),
+    'A#4': mapFile('A#4'),
+    'A#5': mapFile('A#5'),
+    'A#6': mapFile('A#6'),
+    'B2':  mapFile('B2'),
+    'B3':  mapFile('B3'),
+    'B4':  mapFile('B4'),
+    'B5':  mapFile('B5'),
+    'B6':  mapFile('B6'),
+    'C2':  mapFile('C2'),
+    'C3':  mapFile('C3'),
+    'C4':  mapFile('C4'),
+    'C5':  mapFile('C5'),
+    'C6':  mapFile('C6'),
+    'C7':  mapFile('C7'),
+    'C#2': mapFile('C#2'),
+    'C#3': mapFile('C#3'),
+    'C#4': mapFile('C#4'),
+    'C#5': mapFile('C#5'),
+    'C#6': mapFile('C#6'),
+    'D0':  mapFile('D0'),
+    'D1':  mapFile('D1'),
+    'D2':  mapFile('D2'),
+    'D3':  mapFile('D3'),
+    'D4':  mapFile('D4'),
+    'D5':  mapFile('D5'),
+    'D6':  mapFile('D6'),
+    'D#2': mapFile('D#2'),
+    'D#3': mapFile('D#3'),
+    'D#4': mapFile('D#4'),
+    'D#5': mapFile('D#5'),
+    'D#6': mapFile('D#6'),
+    'E2':  mapFile('E2'),
+    'E3':  mapFile('E3'),
+    'E4':  mapFile('E4'),
+    'E5':  mapFile('E5'),
+    'E6':  mapFile('E6'),
+    'F2':  mapFile('F2'),
+    'F3':  mapFile('F3'),
+    'F4':  mapFile('F4'),
+    'F5':  mapFile('F5'),
+    'F6':  mapFile('F6'),
+    'F#2': mapFile('F#2'),
+    'F#3': mapFile('F#3'),
+    'F#4': mapFile('F#4'),
+    'F#5': mapFile('F#5'),
+    'F#6': mapFile('F#6'),
+    'G2':  mapFile('G2'),
+    'G3':  mapFile('G3'),
+    'G4':  mapFile('G4'),
+    'G5':  mapFile('G5'),
+    'G6':  mapFile('G6'),
+    'G#2': mapFile('G#2'),
+    'G#3': mapFile('G#3'),
+    'G#4': mapFile('G#4'),
+    'G#5': mapFile('G#5'),
+    'G#6': mapFile('G#6')
+  }
+
+  SoundLib.load = function (arg) {
+      var t;
+      (arg) ? t = arg: t = {};
+      t.instruments = t.instruments || this.list;
+
+      newT = this[t.instruments];
+          var s = new Tone.Sampler(newT)
+      return s
+  }
+
+  return SoundLib
+
+})()
+
+initPiano()
+bindKeyBoradEvent()
+
+// 钢琴初始化
+async function initPiano() {
+  setTimeout(() => {
+    this.computeEleSize()
+    this.pianoShow = true
+  }, 300)
+  this.bindKeyBoradEvent()
+  this.setListener()
+
+  this.synth = SoundLib.load({
+    instruments:'Piano'
+  }).toMaster()
+  
+}
+
+function computeEleSize() {
+  let wkey_width = $('.piano-key-wrap').width() / 36;
+  let wkey_height = wkey_width * 7;
+  let bkey_height = wkey_height * 0.7;
+  $('.piano-key-wrap').height(wkey_height);
+  $('.bkey').height(bkey_height);
+}
+
+function setListener() {
+  window.onresize = this.computeEleSize
+  window.onorientationchange = this.computeEleSize
+}
+
+// 键盘操作 核心代码
+function bindKeyBoradEvent(){
+  const ShiftKeyCode = 16
+  document.addEventListener('keydown', (e) => {
+    let keyCode = e.keyCode;
+    if (this.DEV) console.log('keydown', keyCode);
+    // 按住Shfit键，则启用黑色按键
+    if (keyCode == ShiftKeyCode) {
+      this.enableBlackKey = true
+    }
+    if (this.enableBlackKey) keyCode = 'b' + keyCode
+
+    if (keyCode == this.lastKeyCode) {
+      // 连续触发同一个键时，应节流 + 延音
+      if (!this.keyLock) {
+        this.playNoteByKeyCode(keyCode)
+        // 这里应该延音，解决中...
+        this.lastKeyCode = keyCode
+        this.keyLock = true
+      }
+      if (this.keydownTimer) {
+        clearTimeout(this.keydownTimer)
+        this.keydownTimer = null
+      }
+      this.keydownTimer = setTimeout(() => {
+        this.keyLock = false
+      }, 120)
+    } else {
+      this.playNoteByKeyCode(keyCode)
+      this.lastKeyCode = keyCode
+    }
+  }, false)
+
+  document.addEventListener('keyup', (e) => {
+    // console.log('keyup');
+    let keyCode = e.keyCode;
+    // 松开Shfit键，则禁用黑色按键
+    if (keyCode == ShiftKeyCode) {
+      this.enableBlackKey = false;
+    }
+    $(`.wkey`).removeClass('wkey-active')
+    $(`.bkey`).removeClass('bkey-active')
+  }, false)
+}
+
+ // 根据键值播放音符
+ function playNoteByKeyCode(keyCode) {
+  let pressedNote = this.getNoteByKeyCode(keyCode)
+  if (pressedNote) {
+    this.playNote(pressedNote.name)
+    let keyType = pressedNote.type;
+    if (keyType == 'white') {
+      $(`[data-keyCode=${pressedNote.keyCode}]`).addClass('wkey-active');
+    } else if (keyType == 'black') {
+      $(`[data-keyCode=${pressedNote.keyCode}]`).addClass('bkey-active');
+    }
+  }
+}
+
+// 触发单个音符播放
+function playNote(notename = 'C4', duration = '1n') {
+  if (!this.synth) return
+  try {
+    this.synth.triggerAttackRelease(notename, duration);
+  } catch (e) {}
+}
+
+function getNoteByKeyCode(keyCode) {
+  // 改为更高性能的写法
+  let target
+  let len = this.Notes.length || 0
+  for (let i = 0; i < len; i++) {
+    let note = this.Notes[i]
+    if (note.keyCode == keyCode) {
+      target = note
+      break
+    }
+  }
+  return target
+}
+
+let wkey = document.getElementsByClassName('wkey')
+let bkey = document.getElementsByClassName('bkey')
+
+bindingMouseClickEvent(wkey)
+bindingMouseClickEvent(bkey)
+
+function bindingMouseClickEvent(type) {
+  for (let i = 0; i < type.length; i++) {   
+    type[i].onclick = function () {
+      for (let j = 0; j < type.length; j++) {
+        clickPianoKey(type[i].getAttribute('data-keycode'))
+      }
+    }
+  }
+}
+
+// 鼠标操作，点击按键播放
+function clickPianoKey(keyCode) {
+let pressedNote = this.getNoteByKeyCode(keyCode)
+  if (pressedNote) {
+    this.playNote(pressedNote.name)
+  }
+}
